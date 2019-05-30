1619f27ab2759ea1f4fb3828c1be7838371a8be0
Peter Christensen <peter.f.chris@gmail.com>
2019-05-29 19:27:18 -0600

    step 7 done

--- src/redux/mediumReducer.js
+++ src/redux/mediumReducer.js
@@ -1,8 +1,36 @@
+import axios from 'axios'
+
+const REQUEST_ARTICLES = 'REQUEST_ARTICLES'
+
+export function requestArticles(){
+    const medium = axios
+    .get('/api/medium').then(res => res.data)
+    return {
+        type: REQUEST_ARTICLES,
+        payload: medium
+    }
+}
+
+function mediumReducer(state = initialState, action){
+    switch(action.type){
+        case REQUEST_ARTICLES + '_PENDING':
+            return {
+                ...state,
+                loading: true
+            }
+        case REQUEST_ARTICLES + '_FULFILLED':
+            return {
+                loading: false,
+                articles: action.payload
+            }
+        default:
+            return state
+    }
+}
+
 const initialState = {
     loading: false,
     articles: []
 }
 
-export default function (state = initialState, action) {
-    return state
-}
+export default mediumReducer
